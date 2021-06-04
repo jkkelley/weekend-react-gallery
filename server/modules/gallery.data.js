1251688ce924d1bedb571d3ea147ac6439e9e572
James Kelley <jkkelley86@gmail.com>
2021-06-04 07:02:12 -0500

    added starting data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    {id: 2, path: 'images/pic_of_bear.png', description: 'Photo of a bear taken at Yellowstone National Park.', likes: 0},
+    {id: 3, path: 'images/sasquatch_unknown.png', description: 'Photo of Sasquatch, Unknown Location.', likes: 0},
+    {id: 4, path: 'images/yeti_unknown.jpeg', description: 'Photo of the mysterious Yeti. Rumor has it, Yeti\'s are from Nepal.', likes: 0},
+    {id: 5, path: 'images/santa-stripe-gremlin.jpeg', description: 'Who needs the Grinch, When Santa Gremlin is here to steal all the presents?', likes: 0},
+    {id: 6, path: 'images/baby_yoda.jpeg', description: 'Don\'t let the cuteness fool yah, this little creature is VERY powerful.', likes: 0}
 ];
 
 module.exports = galleryItems;
+
+// {id: , path: '', description: '', likes: 0}
