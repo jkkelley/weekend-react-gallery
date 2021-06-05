9ef39d285905e5c33f81d6e0106a885018a1a685
James Kelley <jkkelley86@gmail.com>
2021-06-05 13:02:42 -0500

    added to heroku

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -3,17 +3,17 @@ import "./GalleryList.css";
 
 function GalleryList({ gallery, fetchGallery }) {
   return (
-    <>
+
       <div className="gallery-pictures-container">
         {gallery.map((gallery) => (
           <GalleryItem
-            key={gallery.id}
-            gallery={gallery}
             fetchGallery={fetchGallery}
+            gallery={gallery}
+            key={gallery.id}
           />
         ))}
       </div>
-    </>
+
   );
 }
 
