c0713f412f20ff64cdfb9646e493ed15cf24d5e9
James Kelley <jkkelley86@gmail.com>
2021-06-05 13:22:26 -0500

    testing if process.env.database is causing an err

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -27,7 +27,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: 'localhost',
     port: 5432,
-    database: process.env.DATABASE_NAME || 'react_gallery', // CHANGE THIS LINE to match your local database name!
+    database: 'react_gallery', // CHANGE THIS LINE to match your local database name!
   };
 }
 // this creates the pool that will be shared by all other modules
